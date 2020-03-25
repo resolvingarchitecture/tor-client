f3a626b38a695809fe0e8be036b8e225bf0deca6
objectorange <brian@resolvingarchitecture.io>
2020-03-25 08:58:50 -0700

    boxing unnecessary

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ pub struct TORClient {
 }
 
 impl TORClient {
-    pub fn new() -> Box<TORClient> {
-        Box::new(TORClient {
+    pub fn new() -> TORClient {
+        TORClient {
 
-        })
+        }
     }
     pub fn init(&mut self) {
         info!("{}","Initializing TOR Client...");
